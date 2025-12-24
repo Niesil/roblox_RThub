36d87d67bd342515a3a965392be8c5c037974e91
Niesil <142841066+Niesil@users.noreply.github.com>
2025-12-24 19:58:24 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,21 @@
+local GUI = require(game.ReplicatedStorage.ModuleScript)
+local UIS = game:GetService('UserInputService')
+
+local function randomName()
+	return tostring(math.round(Random.new():NextNumber() * 1000000))
+end
+
+local window = GUI.CreateWindow('   RT Hub')
+
+
+local main = window.addCategory(randomName(),'Main')
+
+local movement = window.addCategory(randomName(),'Movement')
+
+local players = window.addCategory(randomName(),'Players')
+
+local visual = window.addCategory(randomName(),'Visual')
+
+local misc = window.addCategory(randomName(),'Misc')
+
+local settings = window.addCategory(randomName(),'Settings')
