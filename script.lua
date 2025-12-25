400a01f6e0aaa44b83434ca4c8a6d8b5ee541724
Niesil <142841066+Niesil@users.noreply.github.com>
2025-12-25 17:01:55 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,25 +1,21 @@
-return function()
-	local GUI = loadstring(game:HttpGet('https://raw.githubusercontent.com/Niesil/roblox_RThub/main/Gui.lua'))()
-	local UIS = game:GetService('UserInputService', true)
+local GUI = loadstring(game:HttpGet('https://raw.githubusercontent.com/Niesil/roblox_RThub/main/Gui.lua'))()
+local UIS = game:GetService('UserInputService', true)
 
-	local function randomName()
-		return tostring(math.round(Random.new():NextNumber() * 1000000))
-	end
-	
-	local window = GUI.CreateWindow('   RT Hub')
-	
-	
-	local main = window.addCategory(randomName(),'Main')
-	
-	local movement = window.addCategory(randomName(),'Movement')
-	
-	local players = window.addCategory(randomName(),'Players')
-	
-	local visual = window.addCategory(randomName(),'Visual')
-
-	local misc = window.addCategory(randomName(),'Misc')
-	
-	local settings = window.addCategory(randomName(),'Settings')
-
-	print('Loaded!')
+local function randomName()
+	return tostring(math.round(Random.new():NextNumber() * 1000000))
 end
+	
+local window = GUI.CreateWindow('   RT Hub')
+	
+	
+local main = window.addCategory(randomName(),'Main')
+	
+local movement = window.addCategory(randomName(),'Movement')
+	
+local players = window.addCategory(randomName(),'Players')
+	
+local visual = window.addCategory(randomName(),'Visual')
+
+local misc = window.addCategory(randomName(),'Misc')
+	
+local settings = window.addCategory(randomName(),'Settings')
